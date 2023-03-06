6d6ecad1371e65b396dd6ca2a3f1c7f286251b5f
dave8git <dave8git@gmail.com>
2023-03-06 21:53:59 +0100

    Fix push to table not working

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const db = require("./db");
+//const db = require("./db");
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const cors = require('cors');
 
 const app = express(); 
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+const db = [
+    { id: "1", author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: "2", author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  ];
+
 app.get('/testimonials', (req, res) => {
     res.json(db);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    res.json(db.find(element => element.id === req.params.id));
+});
+
+app.post('/testimonials', (req, res) => {
+    const author = req.params.author;
+    const text = req.params.text;
+    const id = uuidv4();
+    db.push({id: id, author: author, text: text});
+    res.json({ message: 'OK'});
 });
 
 
